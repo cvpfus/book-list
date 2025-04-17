8da90c8105da5b326447429cc1887d735345112b
cvpfus <47532266+yusufpraditya@users.noreply.github.com>
2025-04-17 15:02:58 +0700

    refactor: skip categoryId if undefined

--- src/app/api/book/route.ts
+++ src/app/api/book/route.ts
@@ -30,7 +30,7 @@ export async function POST(request: NextRequest) {
         author,
         publisher,
         publicationDate,
-        categoryId,
+        categoryId: categoryId ?? Prisma.skip,
       },
     });
 
